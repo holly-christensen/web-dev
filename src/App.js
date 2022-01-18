f3f18cacaea7c94544ec7204554234d8d1147a5b
Holly Christensen <holly.christensen@comcast.net>
2022-01-18 15:13:44 -0500

    change page welcome text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Web Dev Spring 2022 - Holly Lovejoy
         </p>
         <a
           className="App-link"
