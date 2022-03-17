84d62918f6c88e0a5775f998a9eb363e36064ca1
Holly Christensen <holly.christensen@comcast.net>
2022-03-17 15:09:36 -0400

    labs components practice

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
-import logo from './logo.svg';
+import {BrowserRouter, Route, Routes}
+       from "react-router-dom";
 import './App.css';
-import '../vendors/bootstrap/css/bootstrap.min.css';
-import '../vendors/bootstrap/bootstrap.min.css';
-import '../vendors/fontawesome/css/all.min.css';
-
+import './vendors/bootstrap/css/bootstrap.min.css';
+import './vendors/bootstrap/bootstrap.min.css';
+import './vendors/fontawesome/css/all.min.css';
+import HelloWorld from "./components/hello-world";
+import Labs from "./components/labs";
+import Tuiter from "./components/tuiter";
 
 
 
 function App() {
-  return (
-    <div className="container">
-      <h1>Hello World!</h1>
-    </div>
-  );
+ return (
+   <BrowserRouter>
+     <div className="container">
+       <Routes>
+         <Route path="/hello"
+                exact={true}
+                element={<HelloWorld/>}/>
+         <Route path="/labs"
+                exact={true}
+                element={<Labs/>}/>
+         <Route path="/tuiter"
+                exact={true}
+                element={<Tuiter/>}/>
+       </Routes>
+     </div>
+   </BrowserRouter>
+ );
 }
+
 
 
 
---
+++ src/components/hello-world.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const HelloWorld = () => {
+    return(
+        <h1>Hello World!</h1>
+          <Link to="/">
+              Labs
+          </Link> |
+          <Link to="/tuiter">
+              Tuiter
+          </Link>
+
+    )
+};
+
+export default HelloWorld;
+
