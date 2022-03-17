76fe67131311b2cfca0405bb9201ef8cf26dc68d
Holly Christensen <holly.christensen@comcast.net>
2022-03-17 14:27:18 -0400

    copy vendors directory in to src

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import '../vendors/bootstrap/css/bootstrap.min.css';
+import '../vendors/bootstrap/bootstrap.min.css';
+import '../vendors/fontawesome/css/all.min.css';
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Web Dev Spring 2022 - Holly Lovejoy
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Hello World!</h1>
     </div>
   );
 }
+
+
+
+//function App() {
+//  return (
+//    <div className="App">
+//      <header className="App-header">
+//        <img src={logo} className="App-logo" alt="logo" />
+//        <p>
+//          Web Dev Spring 2022 - Holly Lovejoy
+//        </p>
+//        <a
+//          className="App-link"
+//          href="https://reactjs.org"
+//          target="_blank"
+//          rel="noopener noreferrer"
+//        >
+//          Learn React
+//        </a>
+//      </header>
+//    </div>
+//  );
+//}
 
 export default App;
