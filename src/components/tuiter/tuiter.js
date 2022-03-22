a50b6de1c7d224fcbbcaa1f4a511022b8c414f50
Holly Christensen <holly.christensen@comcast.net>
2022-03-21 22:56:33 -0400

    remove whotofollow from tuiter.js

--- src/components/tuiter/tuiter.js
+++ src/components/tuiter/tuiter.js
@@ -1,9 +1,5 @@
 import React from "react";
 import {Link} from "react-router-dom";
-import NavigationSidebar from "./NavigationSidebar/index.js"
-import WhoToFollowListItem from "./WhoToFollowList/WhoToFollowListItem.js"
-import WhoToFollowList from "./WhoToFollowList/WhoToFollowList.js"
-import PostSummaryListItem from "./PostSummaryList/PostSummaryListItem.js"
 import ExploreScreen from "./ExploreScreen/index.js"
 
 const Tuiter = () => {
@@ -11,21 +7,6 @@ const Tuiter = () => {
       <>
             <h1>Tuiter</h1>
             <ExploreScreen/>
-             {/*<PostSummaryListItem post={{
-                 "topic": "Web Development",
-                 "userName": "",
-                 "title": "jQuery",
-                 "time": "last week",
-                 "image": "./images/jquery.png",
-                 "tweets": "122K"
-             }}/>
-            <NavigationSidebar active="home"/>
-            <WhoToFollowListItem who={{
-             avatarIcon: './images/virgin-logo.png',
-             userName: 'Virgin Galactic',
-             handle: 'virgingalactic',
-                         }}/>*/}
-             <WhoToFollowList/>
           <Link to="/hello">
               Hello
           </Link> |
